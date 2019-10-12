60c8d3082782f33cec5fc09063182ec92b1af58e
ayauri29 <albayauri.29@gmail.com>
2019-10-12 14:38:14 -0500

    Header fix

--- src/index.js
+++ src/index.js
@@ -5,43 +5,43 @@ const btnCopy = document.getElementById("btn-copy");
 const btnClean = document.getElementById("btn-clean")
 let intentos = 3;
 
-start.addEventListener("click", () =>{
+start.addEventListener("click", () => {
 
-  const pass=document.getElementById("password").value;
-   if(pass=="LABORATORIA"){
-     ocultar();
-   }else{
-    if(intentos==0)
-    {
-     document.getElementById("password").value = "";
-     document.getElementById("incorrecto").innerHTML = "No te quedan más intentos.";
-     document.getElementById("password").disabled = true;
-    }else{
-     intentos=intentos-1;
-     document.getElementById("password").value = "";
-     document.getElementById("incorrecto").innerHTML = "Te quedan "+intentos + " intentos.";
+  const pass = document.getElementById("password").value;
+  if (pass == "LABORATORIA") {
+    ocultar();
+  } else {
+    if (intentos == 0) {
+      document.getElementById("password").value = "";
+      document.getElementById("incorrecto").innerHTML = "No te quedan más intentos.";
+      document.getElementById("password").disabled = true;
+    } else {
+      intentos = intentos - 1;
+      document.getElementById("password").value = "";
+      document.getElementById("incorrecto").innerHTML = "Te quedan " + intentos + " intentos.";
     }
-   }
+  }
 });
 
-let   ocultar = () => {
+let ocultar = () => {
   document.getElementById('login').classList.add('hide');
- document.getElementById('cifrado').classList.remove('hide');
+  document.getElementById('cifrado').classList.remove('hide');
+  document.getElementById('footer').classList.remove('hide');
 }
 
-btnCipher.addEventListener("click",() => {
+btnCipher.addEventListener("click", () => {
   const texto = document.getElementById("myTexto").value;
   const offSet = parseInt(document.getElementById("offset").value);
   const resultado = document.getElementById("resultado");
-  const cifrado = cipher.encode(offSet,texto);
+  const cifrado = cipher.encode(offSet, texto);
   resultado.value = cifrado;
 });
 
-btnDecipher.addEventListener("click",() => {
+btnDecipher.addEventListener("click", () => {
   const texto = document.getElementById("myTexto").value;
   const offSet = parseInt(document.getElementById("offset").value);
   const resultado = document.getElementById("resultado");
-  const desCifrado = cipher.decode(offSet,texto);
+  const desCifrado = cipher.decode(offSet, texto);
   resultado.value = desCifrado;
 });
 
